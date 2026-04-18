947b027c5c3fecec881c0cfc28680566018d834e
morikei72-max <morikei72@gmail.com>
2026-04-18 18:13:15 +0900

    Clean up stripe-webhook.js by removing comments
    Remove commented code about Base64 encoding and add newline at end of file.

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -10,8 +10,6 @@ exports.handler = async (event) => {
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   const signature = event.headers['stripe-signature'];
 
-  // Netlify FunctionsはBase64エンコードされた状態でbodyが来る場合がある
-  // Stripe署名検証には元のrawバイト列が必要
   const rawBody = event.isBase64Encoded
     ? Buffer.from(event.body, 'base64')
     : Buffer.from(event.body, 'utf8');
